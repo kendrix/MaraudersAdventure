d73cfceae030f8c5e929dc6d0eeea8ea2b7c2484
kendrix <marie.ruffier@gmail.com>
2014-07-02 17:03:20 +0200

    Ajout image de garde

--- DesignPatern/DesignPatern/Properties/Resources.Designer.cs
+++ DesignPatern/DesignPatern/Properties/Resources.Designer.cs
@@ -103,6 +103,26 @@ namespace MaraudersAdventure.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap carte_maraudeur {
+            get {
+                object obj = ResourceManager.GetObject("carte_maraudeur", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap carte_maraudeur1 {
+            get {
+                object obj = ResourceManager.GetObject("carte_maraudeur1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap chaussure {
             get {
                 object obj = ResourceManager.GetObject("chaussure", resourceCulture);
