6733ea51dba71a8da4ea0881b7cd8b16623b6f0b
mperroteau <marine.perroteau@gmail.com>
2014-07-02 16:01:15 +0200

    Projet test unitaire
    Test combattre

---
+++ DesignPatern/Marauder's Adventure Test/Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaraudersAdventure;
+using System.Collections.Generic;
+using MaraudersAdventure;
+
+namespace Marauder_s_Adventure_Test
+{
+    [TestClass]
+    public class Test
+    {
+        List<Personnage> personnagesR = new List<Personnage>();
+        List<Personnage> personnagesV = new List<Personnage>();
+
+        List<string> quetesR = new List<string>();
+        List<string> quetesV = new List<string>();
+
+        Personnage combattantR = new Chevalier("Harry Potter", TypeEquipe.Rouge);
+        Personnage combattantV = new Chevalier("Voldermort", TypeEquipe.Verte);
+
+        MapType map = new MapType();
+
+        private void setSetGameChevaliers()
+        {
+            //Tous les personnages sont des chevaliers
+            personnagesR.Add(combattantR);
+            personnagesR.Add(new Chevalier("Ginny W.", TypeEquipe.Rouge));
+            personnagesR.Add(new Chevalier("Test", TypeEquipe.Rouge));
+
+            personnagesV.Add(combattantV);
+            personnagesV.Add(new Chevalier("Pierre", TypeEquipe.Verte));
+            personnagesV.Add(new Chevalier("Méchant", TypeEquipe.Verte));
+
+            quetesR.Add("Objet");
+            quetesV.Add("Objet");
+
+        }
+
+
+        [TestMethod]
+        public void TestCombattre()
+        {
+            setSetGameChevaliers();
+            ConfigurationGame game = new ConfigurationGame("Equipe rouge", "Equipe verte",
+                    personnagesR, personnagesV, quetesR, quetesV, MapType.standard);
+            //Setter des positions aux personnages
+            Position combattantPosition = new Position(1, 1);
+
+            combattantR.Position = combattantPosition;
+            combattantV.Position = combattantPosition;
+
+            combattantR.Combattre(combattantV);
+
+            Assert.AreEqual(97, combattantV.PointsDeVie);
+
+
+            
+        }
+            
+    }
+}
